8630ba1ef91c7aa5e6d993feed8f4dbcb87e20f1
Tony Levi <tlevi@internode.on.net>
2010-01-02 22:07:17 +1030

    + vector2.h: 2d vector class from old code

---
+++ vector2.h
@@ -0,0 +1,97 @@
+#ifndef VECTOR2_H_
+#define VECTOR2_H_
+
+
+#include <cmath>
+
+
+#define DOT(A,B)		(A.x*B.x+A.y*B.y)
+#define NORMALIZE(A)	{A *= 1.0 / sqrtf(A.x*A.x+A.y*A.y);}
+#define LENGTH(A)		(sqrtf(A.x*A.x+A.y*A.y))
+#define SQRLENGTH(A)	(A.x*A.x+A.y*A.y)
+#define SQRDISTANCE(A,B) ((A.x-B.x)*(A.x-B.x)+(A.y-B.y)*(A.y-B.y))
+
+
+class vector2{
+public:
+	float x;
+	float y;
+
+
+	explicit inline vector2(){};
+
+
+	//If you want it zero'd when you start...try blah = vector3(0) (as per below)
+	inline vector2(const float z) : x(0.0f), y(0.0f){};
+
+
+	//Full init
+	inline vector2(const float X, const float Y) : x(X), y(Y) {};
+
+
+	inline void Normalise(void){
+		const float l = 1.0 / sqrtf(x*x + y*y);
+		x *= l;
+		y *= l;
+	};
+
+
+	const inline float Length(void) const{
+		return sqrtf(x*x + y*y);
+	};
+
+
+	const inline float Dot(vector2& B) const{
+		return x*B.x + y*B.y;
+	};
+
+
+	inline void operator += (const vector2& B){
+		x += B.x;
+		y += B.y;
+	};
+
+
+	inline void operator -= (const vector2& B){
+		x -= B.x;
+		y -= B.y;
+	};
+
+
+	inline void operator *= (const float f){
+		x *= f;
+		y *= f;
+	};
+	inline void operator *= (const vector2& B){
+		x *= B.x;
+		y *= B.y;
+	};
+
+
+	const inline vector2 operator - () const{
+		return vector2(-x, -y);
+	};
+
+
+	const inline friend vector2 operator + (const vector2& v1, const vector2& v2){
+		return vector2(v1.x + v2.x, v1.y + v2.y);
+	};
+
+
+	const inline friend vector2 operator - (const vector2& v1, const vector2& v2){
+		return vector2(v1.x - v2.x, v1.y - v2.y);
+	};
+
+
+	const inline friend vector2 operator * (const vector2& v1, const vector2& v2){
+		return vector2(v1.x * v2.x, v1.y * v2.y);
+	};
+
+
+	const inline friend vector2 operator * (const vector2& v, const float f){
+		return vector2(v.x * f, v.y * f);
+	};
+};
+
+
+#endif /* VECTOR2_H_ */
