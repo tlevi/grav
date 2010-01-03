a4bbec1bad092a6aece2b11617ca388d0f3cf728
Tony Levi <tlevi@internode.on.net>
2010-01-04 01:50:28 +1030

    o shared.h: add my weird typedefs

--- shared.h
+++ shared.h
@@ -1,1 +1,13 @@
+#ifndef SHARED_H_
+#define SHARED_H_
+
 using namespace std;
+
+
+typedef unsigned int uInt;
+typedef unsigned long uLong;
+typedef unsigned short uShort;
+typedef unsigned char uChar;
+
+
+#endif /* SHARED_H_ */
