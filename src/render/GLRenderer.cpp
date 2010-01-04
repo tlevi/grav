9190da7e2887114f5f8f3cc2130e9ea25f422bf8
Tony Levi <tlevi@internode.on.net>
2010-01-04 22:19:27 +1030

    + GLRenderer: (unimplemented) implementation of Renderer, using OpenGL

---
+++ src/render/GLRenderer.cpp
@@ -0,0 +1,11 @@
+#include "GLRenderer.h"
+
+
+GLRenderer::GLRenderer(){
+	// TODO Auto-generated constructor stub
+};
+
+
+GLRenderer::~GLRenderer(){
+	// TODO Auto-generated destructor stub
+};
---
+++ src/render/GLRenderer.h
@@ -0,0 +1,14 @@
+#ifndef GLRENDERER_H_
+#define GLRENDERER_H_
+
+#include "Renderer.h"
+
+
+class GLRenderer: Renderer{
+public:
+	GLRenderer();
+	~GLRenderer();
+};
+
+
+#endif /* GLRENDERER_H_ */
