6562887175987ba53a872c2f6409590ed8025ec5
Tony Levi <tlevi@internode.on.net>
2010-01-09 23:39:20 +1030

    o shared.h: add whitespace

--- src/shared.h
+++ src/shared.h
@@ -1,9 +1,11 @@
 #ifndef SHARED_H_
 #define SHARED_H_
 
+
 #if !defined(API_SDL) && !defined(API_GLUT)
 #error "Neither glut or SDL specified, use -DAPI_GLUT or -DAPI_SDL"
 #endif
+
 
 using namespace std;
 
