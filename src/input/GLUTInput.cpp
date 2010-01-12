c5eb410345ad74a2b913c149dd5bf7213f833266
Tony Levi <tlevi@internode.on.net>
2010-01-12 14:25:10 +1030

    o input/GLUTInput: implementation

--- src/input/GLUTInput.cpp
+++ src/input/GLUTInput.cpp
@@ -1,38 +1,66 @@
 #ifdef API_GLUT
 #include "../shared.h"
 #include "GLUTInput.h"
+#include <GL/freeglut.h>
+
+
+GLUTInput* GLUTInput::instance = NULL;
+
+
+void GLUTInput::keyboardFunc(unsigned char key, int x, int y){
+	if (instance != NULL) instance->receiveKey(key);
+	else fatalError("keyboardFunc called but no GLUTInput instance!\n");
+};
 
 
 GLUTInput::GLUTInput(){
-	// TODO Auto-generated constructor stub
+	instance = this;
+	glutKeyboardFunc(GLUTInput::keyboardFunc);
 };
 
 
 GLUTInput::~GLUTInput(){
-	// TODO Auto-generated destructor stub
+	glutKeyboardFunc(NULL);
+	instance = NULL;
 };
 
 
 const void GLUTInput::Reinitialise(){
-	//TODO
+	glutKeyboardFunc(GLUTInput::keyboardFunc);
 };
 
 
 const void GLUTInput::PumpEvents(){
-	//TODO
 };
 
 
 const bool GLUTInput::hasNext() const{
-	//TODO
-	return false;
+	return !kevlist.empty();
 };
 
 
 const KeyEvent* const GLUTInput::nextEvent(){
 	static KeyEvent ev('\0', 0);
-	//TODO
+
+	ev = kevlist.front();
+	kevlist.pop_front();
+
 	return &ev;
+};
+
+
+const void GLUTInput::receiveKey(unsigned char key){
+	const int glutmods = glutGetModifiers();
+
+	int mods = 0;
+	if (glutmods == 0) mods = KEYMOD_NONE;
+	else{
+		if (glutmods & GLUT_ACTIVE_CTRL) mods |= KEYMOD_CTRL;
+		if (glutmods & GLUT_ACTIVE_ALT) mods |= KEYMOD_ALT;
+		if (glutmods & GLUT_ACTIVE_SHIFT) mods |= KEYMOD_SHIFT;
+	}
+
+	kevlist.push_back(KeyEvent(key, mods));
 };
 
 
--- src/input/GLUTInput.h
+++ src/input/GLUTInput.h
@@ -3,9 +3,23 @@
 #define GLUTINPUT_H_
 
 #include "Input.h"
+#include <list>
+
+using namespace std;
 
 
 class GLUTInput: public Input{
+private:
+	static GLUTInput* instance;
+
+	static void keyboardFunc(unsigned char key, int x, int y);
+
+
+	list<KeyEvent> kevlist;
+
+	const void receiveKey(unsigned char key);
+
+
 public:
 	GLUTInput();
 	~GLUTInput();
