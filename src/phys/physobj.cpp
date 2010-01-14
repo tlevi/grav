61add50106debe6ffecb27ec56b9f52ee3f90034
Tony Levi <tlevi@internode.on.net>
2010-01-14 19:25:19 +1030

    o physobj: implement constructor

--- src/phys/physobj.cpp
+++ src/phys/physobj.cpp
@@ -6,11 +6,15 @@ physobj::physobj(){
 };
 
 
-physobj::physobj(const float mass){
-//TODO: physobj()
+physobj::physobj(const float radius){
+	this->radius = radius;
+	mass = radius * radius * M_PI;
+	p = vector2(0.0f, 0.0f);
+	po = vector2(0.0f, 0.0f);
+	a = vector2(0.0f, 0.0f);
+	invmass = 1.0 / mass;
 };
 
 
 physobj::~physobj(){
-	//TODO: ~physobj()
 };
