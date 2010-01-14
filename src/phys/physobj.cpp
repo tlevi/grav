338e402585b74aabc5aa38751f6bf45c49da9504
Tony Levi <tlevi@internode.on.net>
2010-01-15 02:39:09 +1030

    o physobj: increase object mass, fix broken GLUT compile

--- src/phys/physobj.cpp
+++ src/phys/physobj.cpp
@@ -1,6 +1,7 @@
 #include "physobj.h"
-#include <SDL.h>
 #include "../shared.h"
+
+typedef unsigned int Uint32;
 
 
 unsigned int physobj::nextid = 1;
@@ -8,7 +9,7 @@ unsigned int physobj::nextid = 1;
 
 physobj::physobj(const float aRadius) : id(nextid++){
 	radius = aRadius;
-	mass = radius * radius * M_PI;
+	mass = radius * radius * M_PI * 4.0;
 	p = vector2(0.0f, 0.0f);
 	po = vector2(0.0f, 0.0f);
 	a = vector2(0.0f, 0.0f);
